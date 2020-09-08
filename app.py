9408a53c9345ce369dbf394c8b2c70c9d4f0530a
Sławomir Brzozowski <brzozasr@gmail.com>
2020-09-08 15:41:28 +0200

    Update: index.html, app.py, style.css, list.html, pattern.html, question.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 from database_tools import *
 from query import *
 
@@ -14,20 +14,21 @@ def index():
 @app.route('/list')
 def question_list():
     questions_list = db.execute_sql(query.question_select_all_desc_by_date)
-    header_table = ['ID', 'Date', 'Views', 'Vote', 'Title', 'Question', 'Image']
-    # TODO sort in query for DB
-    return render_template('list.html', header_table=header_table, questions=questions_list)
+    cols_to_show = {'Date': 1, 'View': 2, 'Vote': 3, 'Title': 4, 'Question': 5}
+    return render_template('list.html', questions=questions_list, cols=cols_to_show)
 
 
 @app.route('/question/<int:question_id>')
 def question_view(question_id):
-    quest_answers = db.execute_sql(query.question_select_all_desc_by_date)
-    return render_template('question.html', quest_answers=quest_answers, question_id=question_id)
+    view_counter = db.execute_sql(f"SELECT view_number FROM question WHERE id = {question_id}")
+    db.execute_sql(f"UPDATE question SET view_number = {view_counter[0][0] + 1} WHERE id = {question_id}")
+    question = db.execute_sql(f"SELECT * FROM question WHERE id = {question_id}")
+    answer = db.execute_sql(f"SELECT * FROM answer WHERE question_id = {question_id}")
+    return render_template('question.html', question=question, answer=answer)
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    # TODO html page
+def page_not_found():
     return render_template('404.html'), 404
 
 
